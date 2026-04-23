b03b86489d5e62cbd457946f4886059294012973
robbitri1 <robbitri1@gmail.com>
2026-04-23 12:05:19 +0700

    Add Strapi CMS backend with content types for all sections + registration

---
+++ backend/src/api/curriculum-section/controllers/curriculum-section.ts
@@ -0,0 +1,3 @@
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::curriculum-section.curriculum-section');
---
+++ backend/src/api/curriculum-section/services/curriculum-section.ts
@@ -0,0 +1,3 @@
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::curriculum-section.curriculum-section');
